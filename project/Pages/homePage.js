d30335fe6dfa4d0c1f2826270a38df9801bbf3f8
SVishal007 <sonkusare.vishal1@gmail.com>
2020-04-27 10:50:23 +0530

    This was just for test

---
+++ project/Pages/homePage.js
@@ -0,0 +1,8 @@
+let HomePage = function(){
+
+    let firstNumber_Input = element(by.model('first'));
+    let secontNumber_Input = element(by.model('second'));
+    element(by.css('[ng-click="doAddition()"]')).click();
+
+
+};
